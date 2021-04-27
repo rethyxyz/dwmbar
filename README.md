cf71e11f06d18988e5e8ef67710872f71e9239f2
Brody Rethy <brodyrethy@gmail.com>
2021-04-27 14:51:29 -0300

    Source code modifications, and changes to README.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dwmbar
-My status bar for dwm. Written in Bash. Now supports color if your dwm build contains the <a href="https://dwm.suckless.org/patches/statuscolors/">statuscolor</a> patch.
+My status bar for dwm. Written in Bash. Now supports color if your dwm build contains the <a href="https://dwm.suckless.org/patches/statuscolors/">statuscolors</a> patch.
+
+## Usage
+Simply change the **DEVICE**, **INTERFACE**, and **SEPARATOR** (optional) variables to fit your system.
+
+Defaults are set to
+- **INTERFACE**="enp2s0"
+- **DEVICE**="desktop"
+- **SEPARATOR**="╬"
--- dwmbar.sh
+++ dwmbar.sh
@@ -1,63 +1,40 @@
 #!/bin/bash
 
 #
-## By: Brody Rethy
-## Website: https://rethy.xyz
+# By: Brody Rethy
+# Website: https://rethy.xyz
 #
-## Name: dwmbar.sh
+# Name: dwmbar.sh
 #
-## Summary:
-## An extensible status bar for dwm.
-#
-
-#
-# TODO Get args in a loop
-# TODO If needed ones empty (AKA not given) exit, or prompt again.
+# Summary:
+# An extensible status bar for dwm.
 #
 
-display_help() {
-	/usr/bin/echo "dwmbar.sh [DEVICE]"
-	/usr/bin/echo ""
-	/usr/bin/echo "Takes one of two args: laptop, or desktop."
-	/usr/bin/echo "To start this upon dwm launch, put this inside your ~/.xinitrc with the [DEVICE] arg (as shown above)."
-}
+# Insert your network interface here.
+INTERFACE="enp2s0"
+# Insert your device type (laptop or desktop) here.
+DEVICE="desktop"
+# You can change this to any symbol, as long as your font supports it.
+SEPARATOR="╬"
 
-DEVICE=$1
-
-# DEFINE VARS FOR DEVICE
-case $DEVICE in
-	laptop | Laptop | e550 | E550) INTERFACE="wlp4s0" ;;
-	desktop | Desktop) INTERFACE="enp2s0" ;;
-	-h | --help) display_help; exit 0 ;;
-	*)
-		/usr/bin/echo ":: Not a valid device"
-		/usr/bin/echo "::"
-		/usr/bin/echo ":: Type dwmbar.sh -h for assistance"
-		exit 1
-		;;
-esac
+get_date() { date +'%m-%d'; }
+get_time() { date +"%r"; }
 
 get_song_left() {
 	STATE=$(/usr/bin/mpc -p 6601 | sed -n 2p | awk '{print $1}')
 	TIME_REMAINING=$(/usr/bin/mpc -p 6601 | sed -n 2p | awk '{print $3}')
 
-	## Theoretically faster than an if statement, though I haven't tested it yet.
-	case $STATE in
+	case "$STATE" in
 		"[paused]") /usr/bin/echo -e "[PAUSED]" ;;
 		*) /usr/bin/echo -e "$TIME_REMAINING" ;;
 	esac
-
-#	if i[ "$STATE" = "[paused]" ]]
-#	then
-#	else
-#	fi
 }
 
 get_vol() {
    	VOL=$(pulsemixer --get-volume | awk '{print $2}')
 	VOL_STATE=$(pulsemixer --get-mute)
 
-	case $VOL_STATE in
+	case "$VOL_STATE" in
 		1) /usr/bin/echo -e "[MUTED]" ;;
 
 		*)
@@ -72,20 +49,13 @@ get_vol() {
 			fi
 			;;
 	esac
-
-#if [[ "$VOL_STATE" = 1 ]]
-#then
-#else
-#fi
 }
 
-# TODO Check battery state
 get_bat() {
-	# Make charging green in the future
 	BAT_LEVEL="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)"
 	STATUS="$(cat /sys/class/power_supply/BAT*/status)"
 
-	case $STATUS in
+	case "$STATUS" in
 		Charging)
 			/usr/bin/echo "[C] $BAT_LEVEL%"
 			;;
@@ -101,20 +71,15 @@ get_bat() {
 			fi
 			;;
 	esac
-
-#if [[ "$STATUS" = "Charging" ]]
-#then
-#else
-#fi
 }
 
 get_mem_free() {
 	MEM_FREE=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
 
-	if [[ $MEM_FREE -gt 2500 ]]
+	if [[ "$MEM_FREE" -gt 2500 ]]
 	then
 		/usr/bin/echo -e "$MEM_FREE"MB
-	elif [[ $MEM_FREE -gt 1500 ]]
+	elif [[ "$MEM_FREE" -gt 1500 ]]
 	then
 		/usr/bin/echo -e "$MEM_FREE""MB"
 	else
@@ -152,14 +117,8 @@ get_song() {
 	/usr/bin/echo -e "$SONG_TITLE"
 }
 
-get_date() { date +'%m-%d'; }
-
-get_time() { date +"%r"; }
-
-SEPARATOR="╬"
-
 case $DEVICE in
-	laptop)
+	laptop | Laptop)
 		while true
 		do
 			xsetroot -name "$(get_song_left) $(get_song) $SEPARATOR $(get_vol) $SEPARATOR $(get_mem_free) $SEPARATOR $(get_temp) $SEPARATOR $(get_ip_addr) $SEPARATOR $(get_bat) $SEPARATOR $(get_date) $SEPARATOR $(get_time)"
@@ -167,16 +126,14 @@ case $DEVICE in
 		done
 		;;
 
-	desktop)
+	desktop | Desktop)
 		while true
 		do
 			xsetroot -name "$(get_song_left) $(get_song) $SEPARATOR $(get_vol) $SEPARATOR $(get_mem_free) $SEPARATOR $(get_ip_addr) $SEPARATOR $(get_date) $SEPARATOR $(get_time)"
 			sleep 0.2
 		done
 		;;
+	*)
+		echo ":: Not a valid device"
+		;;
 esac
-
-#if [[ "$DEVICE" = "laptop" ]]
-#then
-#else
-#fi
