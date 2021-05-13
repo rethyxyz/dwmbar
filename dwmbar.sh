a96986c10f487a127e5dbf00411f389ffc2e5828
Brody Rethy <brodyrethy@gmail.com>
2021-05-13 00:36:16 -0300

    More code clean up.

--- dwmbar.sh
+++ dwmbar.sh
@@ -11,11 +11,11 @@
 # the code below.
 #
 
-# Insert your network interface here.
+# Your primary network interface goes here.
 INTERFACE="enp2s0"
-# Insert your device type (laptop or desktop) here.
+# Your device type (laptop or desktop) goes here (displays bat info if laptop).
 DEVICE="desktop"
-# You can change this to any symbol, as long as your font supports it.
+# This can be any character, as long as your font supports it.
 SEPARATOR="╬"
 
 get_time() { date +"%r"; }
@@ -23,99 +23,99 @@ get_date() { date +'%m-%d'; }
 get_mpd_track() { /usr/bin/mpc -p 6601 current; }
 
 get_mpd_remaining() {
-	STATE=$(/usr/bin/mpc -p 6601 | sed -n 2p | awk '{print $1}')
-	TIME_REMAINING=$(/usr/bin/mpc -p 6601 | sed -n 2p | awk '{print $3}')
+    STATE=$(/usr/bin/mpc -p 6601 | sed -n 2p | awk '{print $1}')
+    TIME_REMAINING=$(/usr/bin/mpc -p 6601 | sed -n 2p | awk '{print $3}')
 
-	case "$STATE" in
-		"[paused]") /usr/bin/echo -e "[PAUSED]" ;;
-		*) /usr/bin/echo -e "$TIME_REMAINING" ;;
-	esac
+    case "$STATE" in
+        "[paused]") /usr/bin/echo -e "[PAUSED]" ;;
+        *) /usr/bin/echo -e "$TIME_REMAINING" ;;
+    esac
 }
 
 get_vol_perc() {
-   	VOL=$(pulsemixer --get-volume | awk '{print $2}')
-	VOL_STATE=$(pulsemixer --get-mute)
+    VOL=$(pulsemixer --get-volume | awk '{print $2}')
+    VOL_STATE=$(pulsemixer --get-mute)
 
-	case "$VOL_STATE" in
-		1) /usr/bin/echo -e "[MUTED]" ;;
-		*)
-			if [ "$VOL" -lt 25 ]; then
-				/usr/bin/echo -e "$VOL%"
-			elif [ "$VOL" -lt 50 ]; then
-				/usr/bin/echo -e "$VOL%"
-			else
-				/usr/bin/echo -e "$VOL%"
-			fi
-			;;
-	esac
+    case "$VOL_STATE" in
+        1) /usr/bin/echo -e "[MUTED]" ;;
+        *)
+            if [ "$VOL" -lt 25 ]; then
+                /usr/bin/echo -e "$VOL%"
+            elif [ "$VOL" -lt 50 ]; then
+                /usr/bin/echo -e "$VOL%"
+            else
+                /usr/bin/echo -e "$VOL%"
+            fi
+            ;;
+    esac
 }
 
 get_bat_perc() {
-	BAT_LEVEL="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)"
-	STATUS="$(cat /sys/class/power_supply/BAT*/status)"
+    BAT_LEVEL="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)"
+    STATUS="$(cat /sys/class/power_supply/BAT*/status)"
 
-	case "$STATUS" in
-		Charging) /usr/bin/echo "[C] $BAT_LEVEL%" ;;
-		*)
-			if [ "$BAT_LEVEL" -lt 25 ]; then
-				/usr/bin/echo "$BAT_LEVEL%"
-			elif [ "$BAT_LEVEL" -lt 50 ]; then
-				/usr/bin/echo "$BAT_LEVEL%"
-			else
-				/usr/bin/echo "$BAT_LEVEL"%
-			fi
-			;;
-	esac
+    case "$STATUS" in
+        Charging) /usr/bin/echo "[C] $BAT_LEVEL%" ;;
+        *)
+            if [ "$BAT_LEVEL" -lt 25 ]; then
+                /usr/bin/echo "$BAT_LEVEL%"
+            elif [ "$BAT_LEVEL" -lt 50 ]; then
+                /usr/bin/echo "$BAT_LEVEL%"
+            else
+                /usr/bin/echo "$BAT_LEVEL"%
+            fi
+            ;;
+    esac
 }
 
 get_mem_free() {
-	MEM_FREE=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
+    MEM_FREE=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
 
-	if [ "$MEM_FREE" -gt 2500 ]; then
-		/usr/bin/echo -e "$MEM_FREE"MB
-	elif [ "$MEM_FREE" -gt 1500 ]; then
-		/usr/bin/echo -e "$MEM_FREE""MB"
-	else
-		/usr/bin/echo -e "$MEM_FREE""MB"
-	fi
+    if [ "$MEM_FREE" -gt 2500 ]; then
+        /usr/bin/echo -e "$MEM_FREE"MB
+    elif [ "$MEM_FREE" -gt 1500 ]; then
+        /usr/bin/echo -e "$MEM_FREE""MB"
+    else
+        /usr/bin/echo -e "$MEM_FREE""MB"
+    fi
 }
 
 get_temp() {
-	TEMP=$(head -c 2 /sys/class/thermal/thermal_zone0/temp)
+    TEMP=$(head -c 2 /sys/class/thermal/thermal_zone0/temp)
 
-	if [ "$TEMP" -gt 80 ]; then
-		/usr/bin/echo -e "$TEMP""C"
-	elif [ "$TEMP" -gt 60 ]; then
-		/usr/bin/echo -e "$TEMP""C"
-	else
-		/usr/bin/echo -e "$TEMP""C"
-	fi
+    if [ "$TEMP" -gt 80 ]; then
+        /usr/bin/echo -e "$TEMP""C"
+    elif [ "$TEMP" -gt 60 ]; then
+        /usr/bin/echo -e "$TEMP""C"
+    else
+        /usr/bin/echo -e "$TEMP""C"
+    fi
 }
 
 get_ip_addr() {
-	IP_ADDR=$(ip addr | awk "/$INTERFACE/ && /inet/" | awk '{print $2}')
+    IP_ADDR=$(ip addr | awk "/$INTERFACE/ && /inet/" | awk '{print $2}')
 
-	if [ "$IP_ADDR" ]; then
-		/usr/bin/echo -e "$IP_ADDR"
-	else
-		/usr/bin/echo -e "[OFFLINE]"
-	fi
+    if [ "$IP_ADDR" ]; then
+        /usr/bin/echo -e "$IP_ADDR"
+    else
+        /usr/bin/echo -e "[OFFLINE]"
+    fi
 }
 
 case $DEVICE in
-	laptop | Laptop)
-		while true; do
-			xsetroot -name "$(get_mpd_remaining) $(get_mpd_track) $SEPARATOR $(get_vol_perc) $SEPARATOR $(get_mem_free) $SEPARATOR $(get_temp) $SEPARATOR $(get_ip_addr) $SEPARATOR $(get_bat_perc) $SEPARATOR $(get_date) $SEPARATOR $(get_time)"
-			sleep 0.2
-		done
-		;;
+    laptop | Laptop)
+        while true; do
+            xsetroot -name "$(get_mpd_remaining) $(get_mpd_track) $SEPARATOR $(get_vol_perc) $SEPARATOR $(get_mem_free) $SEPARATOR $(get_temp) $SEPARATOR $(get_ip_addr) $SEPARATOR $(get_bat_perc) $SEPARATOR $(get_date) $SEPARATOR $(get_time)"
+            sleep 0.2
+        done
+        ;;
 
-	desktop | Desktop)
-		while true; do
-			xsetroot -name "$(get_mpd_remaining) $(get_mpd_track) $SEPARATOR $(get_vol_perc) $SEPARATOR $(get_mem_free) $SEPARATOR $(get_ip_addr) $SEPARATOR $(get_date) $SEPARATOR $(get_time)"
-			sleep 0.2
-		done
-		;;
+    desktop | Desktop)
+        while true; do
+            xsetroot -name "$(get_mpd_remaining) $(get_mpd_track) $SEPARATOR $(get_vol_perc) $SEPARATOR $(get_mem_free) $SEPARATOR $(get_ip_addr) $SEPARATOR $(get_date) $SEPARATOR $(get_time)"
+            sleep 0.2
+        done
+        ;;
 
-	*) echo ":: Not a valid device" ;;
+    *) echo ":: Not a valid device" ;;
 esac
